1ab1a0bf96cadf3efc93d93c590256462a2828c5
Jakob Gamper <97gamjak@gmail.com>
2023-10-21 00:19:04 +0200

    trying out some fixture stuff

--- tests/integrationTests/testsets/test.py
+++ tests/integrationTests/testsets/test.py
@@ -1,18 +1,19 @@
 import pytest
 import utils.read as read
+import utils.filesystem as fs
+import utils.execute as execute
 
 
 class TestMethod:
 
     i = 1
 
-    @classmethod
-    def setup_class(cls):
-        cls.i += 1
+    @pytest.fixture(scope="class", autouse=True)
+    def setup(self, executable):
+        self.i += 1
+        yield
+        print("teardown_class called")
 
-    def test_test(self, executable):
+    def test_test(self):
+        print("hallo")
         assert read.func() == self.i
-
-    @classmethod
-    def teardown_class(cls):
-        cls.i -= 1
